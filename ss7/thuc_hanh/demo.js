bf3961580d2eea008ff072f69de72aecf7f245a4
hoangviet2006 <hoangviet30032006@gmail.com>
2024-11-14 09:18:04 +0700

    nộp bài

---
+++ ss7/thuc_hanh/demo.js
@@ -0,0 +1,6 @@
+let inputWidth = prompt("Enter the width")
+let inputHeight = prompt("Enter the Height")
+let Width = parseInt(inputWidth)
+let Height = parseInt(inputHeight)
+let area = Width*Height
+document.write('This isarerea: ' +area)
